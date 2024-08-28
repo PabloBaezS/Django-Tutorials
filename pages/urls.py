0356b0161db3239ce708f7478b45695c5f2583d8
Pablo Baez Santamaria <pbaezs@eafit.edu.co>
2024-08-27 19:25:47 -0500

    Final Commit Tutorial6 (Dependencias)

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView, \
-    ProductIndexView, ProductShowView, ProductCreateView
-
+    ProductIndexView, ProductShowView, ProductCreateView, CartView, \
+    CartRemoveAllView, ImageViewFactory, ImageViewNoDI
+from .utils import ImageLocalStorage
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -11,4 +12,16 @@ urlpatterns = [
     path('products/created/', ProductCreateView.as_view(),
          name='product-create'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(),
+         name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(),
+         name='image_save'),
+
+    # No Dependency
+
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('imagenotdi/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, request
 from django.shortcuts import get_object_or_404  # here by default
 from django.urls import reverse
 from django.views import View
@@ -128,3 +128,83 @@ class ProductListView(ListView):
 
 class ProductCreateSuccessView(TemplateView):
     template_name = 'products/products.html'
+
+class CartView(View):
+    template_name = 'cart/index.html'
+
+    def get(self, request):
+        # Simulated database for products
+        products = {}
+        products[121] = {'name': 'Tv samsung', 'price': '1000'}
+        products[11] = {'name': 'Iphone', 'price': '2000'}
+
+        # Get cart products from session
+        cart_products = {}
+        cart_product_data = request.session.get('cart_product_data', {})
+
+        for key, product in products.items():
+            if str(key) in cart_product_data.keys():
+                cart_products[key] = product
+
+        # Prepare data for the view
+        view_data = {
+            'title': 'Cart - Online Store',
+            'subtitle': 'Shopping Cart',
+            'products': products,
+            'cart_products': cart_products
+        }
+
+        return render(request, self.template_name, view_data)
+
+    def post(self, request, product_id):
+        # Get cart products from session and add the new product
+        cart_product_data = request.session.get('cart_product_data', {})
+        cart_product_data[product_id] = product_id
+        request.session['cart_product_data'] = cart_product_data
+
+        return redirect('cart_index')
+
+
+class CartRemoveAllView(View):
+    def post(self, request):
+        # Remove all products from cart in session
+        if 'cart_product_data' in request.session:
+            del request.session['cart_product_data']
+
+        return redirect('cart_index')
+
+
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'images/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+
+    return ImageView
+
+
+# Without dependency inversion
+
+from django.shortcuts import render, redirect
+from django.views import View
+from .utils import ImageLocalStorage
+
+class ImageViewNoDI(View):
+    template_name = 'imagesnotdi/index.html'
+
+    def get(self, request):
+        image_url = request.session.get('image_url', '')
+        return render(request, self.template_name, {'image_url': image_url})
+
+    def post(self, request):
+        image_storage = ImageLocalStorage()
+        image_url = image_storage.store(request)
+        request.session['image_url'] = image_url
+        return redirect('imagenodi_index')
